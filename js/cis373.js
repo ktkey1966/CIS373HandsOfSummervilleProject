b9266c67545c80421d017cda7aaa36ed3a9bd264
KK <165699745+ktkey1966@users.noreply.github.com>
2025-03-15 08:39:58 -0700

    Add files via upload

--- js/cis373.js
+++ js/cis373.js
@@ -1,35 +1,34 @@
 // JavaScript source code
-        //< !--Internal JavaScript-- >        < !--Modified KKey 3 / 1 / 2025 -- >
-    <script type="text/javascript">
+//< !--Internal JavaScript-- >        < !--Modified KKey 3 / 1 / 2025 -- >
         function validateForm() {
             //Modified KKey 3/8/25 
             let x1 = document.forms["contactForm"]["name"].value;
-        if (x1 == "") {
-            alert("Name must be filled in");
-        return false;
+            if (x1 == "") {
+                alert("Name must be filled in");
+            return false;
+                    }
+            let x2 = document.forms["contactForm"]["email"].value;
+            if (x2 == "") {
+                alert("Email must be filled in");
+            return false;
+                    }
+            let mailformat = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2, 3}');
+            if (!document.forms["contactForm"]["email"].value.match(mailformat))
+            {
+                alert("You have entered an invalid email address");
+            return false;
+                    }
+            let x3 = document.forms["contactForm"]["phone"].value;
+            if (x3 == "") {
+                alert("Phone must be filled in");
+            return false;
+                    }
+            let x4 = document.forms["contactForm"]["message"].value;
+            if (x4 == "") {
+                alert("Message must be filled in");
+            return false;
+                    }
                 }
-        let x2 = document.forms["contactForm"]["email"].value;
-        if (x2 == "") {
-            alert("Email must be filled in");
-        return false;
-                }
-        let mailformat = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2, 3}');
-        if (!document.forms["contactForm"]["email"].value.match(mailformat))
-        {
-            alert("You have entered an invalid email address");
-        return false;
-                }
-        let x3 = document.forms["contactForm"]["phone"].value;
-        if (x3 == "") {
-            alert("Phone must be filled in");
-        return false;
-                }
-        let x4 = document.forms["contactForm"]["message"].value;
-        if (x4 == "") {
-            alert("Message must be filled in");
-        return false;
-                }
-            }
         function getRadioValue(radio) {
             alert("You selected " + radio.value);
             }
@@ -42,5 +41,4 @@
         function getTextboxEntry(textbox) {
             alert("You selected " + textbox.value);
             }
-    </script>
 
